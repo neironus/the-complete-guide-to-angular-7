a6c8ba74bef6a3ad76c363c856943488e3d9226a
DELL-E6540\Max <dakalm@gmail.com>
2018-11-07 05:48:53 +0200

    import { FormsModule } make test ngModel

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-first-app';
+  title = 'my app';
+  name = '';
+  resetName(): void {
+    this.name = '';
+  }
 }
