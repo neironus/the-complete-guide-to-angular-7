1b26edb912ef246c6c977d14a2e2dc8cdb2e2722
DELL-E6540\Max <dakalm@gmail.com>
2018-11-07 23:40:23 +0200

    added project ng-book2

---
+++ angular-hello-world/src/app/hello-world/hello-world.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hello-world',
+  templateUrl: './hello-world.component.html',
+  // template: `
+  //   <p>Hello</p>
+  // `
+  styleUrls: ['./hello-world.component.css']
+})
+export class HelloWorldComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
