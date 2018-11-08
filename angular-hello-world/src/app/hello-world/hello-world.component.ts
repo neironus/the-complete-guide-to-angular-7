f0cbe2344f8f5a883680344af47ef82947845d5d
DELL-E6540\Max <dakalm@gmail.com>
2018-11-08 22:34:05 +0200

    component user-item

--- angular-hello-world/src/app/hello-world/hello-world.component.ts
+++ angular-hello-world/src/app/hello-world/hello-world.component.ts
@@ -2,10 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-hello-world',
-  templateUrl: './hello-world.component.html',
-  // template: `
-  //   <p>Hello</p>
-  // `
+  templateUrl: './hello-world.component.html', //or  template: `<p>Hello</p>`
   styleUrls: ['./hello-world.component.css']
 })
 export class HelloWorldComponent implements OnInit {
